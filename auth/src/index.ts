22ac9a4ca08ce95d5f7943aa99f02e8644cfd904
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-16 20:13:30 +0100

    configuring my orchestration

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,10 @@ import express from 'express'
 import { json } from 'body-parser'
 const app = express();
 app.use(json());
+app.get('api/userS/currentuser', (req, res) => {
+  res.send('Hi there!')
+})
+
 app.listen(3000, () => {
   console.log('listening on port 3000');
   
