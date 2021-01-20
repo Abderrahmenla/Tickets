f0399c5a056a300cfd6012825b3d637e9a69796c
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-20 13:09:19 +0100

    encoding more information in an error && adding express-validator

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,10 @@
-import {Request,Response,NextFunction} from 'express'
+import { Request, Response, NextFunction } from 'express'
+import {RequestValidationError} from '../errors/request-validation-errors'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+
 export const errorHandler = (err: Error, req:Request, res:Response, next:NextFunction) => {
-  console.log('Something went wrong',err);
+  if (err instanceof RequestValidationError) console.log('handling this error as a request validation error');
+  if (err instanceof DatabaseConnectionError) console.log('handling this error as a DB connection error');
   res.status(400).send({
     message: err.message
   })
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express,{Request,Response} from 'express'
 import { body, validationResult } from 'express-validator'
+import {RequestValidationError} from '../errors/request-validation-errors'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 const router = express.Router();
 
@@ -8,9 +10,10 @@ router.post('/api/users/signup', [
   body('password').trim().isLength({min:4,max:20}).withMessage('Password must be between 4 and 20 characters'),
 ], (req: Request, res: Response) => {
   const errors=validationResult(req);
-  if (!errors.isEmpty()) throw new Error('Invalid email or password');
+  if (!errors.isEmpty()) throw new RequestValidationError(errors.array())
   const { email, password } = req.body;
-  console.log('Creating a user...');
+    console.log('Creating a user...');
+    throw new DatabaseConnectionError()
   res.send({});
 });
 // yolo
