74fc895b3e7e7ef556029cae96fc8275ac9669ea
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-21 23:22:48 +0100

    configuring mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
+
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
@@ -10,6 +12,7 @@ import { NotFoundError } from './errors/not-found-error'
 
 const app = express();
 app.use(json());
+
 app.use(errorHandler);
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -18,6 +21,21 @@ app.use(signupRouter);
 app.all('*', async(req,res) => {
   throw new NotFoundError();
 })
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });  
+  } catch (err) {
+    console.error(err);
+    
+  }
+}
+
 app.listen(3000, () => {
   console.log('listening on port 3000');
 })
+start();
