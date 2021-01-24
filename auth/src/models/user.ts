e398ada4b2ae3b8c4d740bb23738ffd16a5079ba
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-24 06:23:24 +0100

    creating user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -5,7 +5,18 @@ interface UserAttrs{
   email: string;
   password: string;
 }
+// An interface that describes the properties 
+// that a User Model has
 
+interface UserModel extends mongoose.Model<UserDoc>{
+  build(attrs: UserAttrs): UserDoc;
+}
+// An interface that describes the propertues
+// that a User Document has
+interface UserDoc extends mongoose.Document{
+  email: string;
+  password: string;
+}
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -16,11 +27,13 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
-//yolo 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs:UserAttrs) => {
+userSchema.statics.build = (attrs:UserAttrs) => {
   return new User(attrs)
 }
+const User = mongoose.model<UserDoc,UserModel>('User', userSchema);
 
+User.build({
+  email: "test@test.com",
+  password: "ypmp"
+})
 export { User };
