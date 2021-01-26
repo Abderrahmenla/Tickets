9f239156fe574f4d1659632da26d1bdbcb51780f
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-26 21:49:42 +0100

    hashing the password

--- auth/src/errors/bad-request-error.ts
+++ auth/src/errors/bad-request-error.ts
@@ -1,15 +1,15 @@
-import { CustomError } from './custom-error'
-export class BadRequestError extends CustomError{
+import { CustomError } from './custom-error';
+
+export class BadRequestError extends CustomError {
   statusCode = 400;
-  constructor(public message:string) {
+
+  constructor(public message: string) {
     super(message);
+
     Object.setPrototypeOf(this, BadRequestError.prototype);
   }
+
   serializeErrors() {
-    return [
-      {
-        message:this.message
-      }
-    ]
+    return [{ message: this.message }];
   }
 }
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -9,10 +9,14 @@ router.post('/api/users/signup', [
   body('email').isEmail().withMessage('Email must be valid'),
   body('password').trim().isLength({min:4,max:20}).withMessage('Password must be between 4 and 20 characters'),
 ], (req: Request, res: Response) => {
-  const errors=validationResult(req);
-  if (!errors.isEmpty()) throw new RequestValidationError(errors.array())
+    
+  const errors = validationResult(req);
+    
+  if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
+    
   const { email,password } = req.body;
   const existingUser = await User.findOne({ email });
+    
   if (existingUser) throw new BadRequestError('Email in use')
   const user = User.build({ email, password }) 
   await user.save();  
