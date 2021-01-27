8e1c1414db03fa9f37b8779ba604802d27141247
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-27 13:07:00 +0100

    implementing jwt key

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -30,6 +30,7 @@ app.all('*', async(req,res) => {
 })
 
 const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -26,7 +26,7 @@ router.post('/api/users/signup', [
     const userJwt = jwt.sign({
       id: user.id,
       email:user.email
-    }, 'asdf')  
+    }, process.env.JWT_KEY!)  
     // store it on a session object
     req.session = {
       jwt: userJwt
