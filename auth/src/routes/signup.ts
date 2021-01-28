81c55cbb383e0845554a1f1908e51b95d618b380
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-28 02:05:14 +0100

    signin logic

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
+import { body } from 'express-validator'
 
-import { body, validationResult } from 'express-validator'
-import {RequestValidationError} from '../errors/request-validation-errors'
+import {validateRequest} from '../middlewares/validate-request'
 import { User } from '../models/user'
 import {BadRequestError} from '../errors/bad-request-error'
 const router = express.Router();
@@ -10,12 +10,7 @@ const router = express.Router();
 router.post('/api/users/signup', [
   body('email').isEmail().withMessage('Email must be valid'),
   body('password').trim().isLength({min:4,max:20}).withMessage('Password must be between 4 and 20 characters'),
-], (req: Request, res: Response) => {
-    
-  const errors = validationResult(req);
-    
-  if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
-    
+],validateRequest,async (req: Request, res: Response) => {
   const { email,password } = req.body;
   const existingUser = await User.findOne({ email });
     
