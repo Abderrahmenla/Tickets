08d203a770bbe20d86080ac2f95e4620cc4952f1
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-28 03:56:47 +0100

    MW to reject the req if U not logged in

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,6 +1,6 @@
 import { Request, Response,NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
-import { isExpressionStatement } from 'typescript';
+
 
 interface UserPayLoad{
   id: string;
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import {currentUser} from '../middlewares/current-user'
+import { currentUser } from '../middlewares/current-user'
+import {requireAuth} from '../middlewares/require-auth'
 
 const router = express.Router();
-router.get('/api/users/currentuser',currentUser, (req,res) => res.send({currentuser:req.currentUser || null}));
+router.get('/api/users/currentuser',currentUser,requireAuth, (req,res) => res.send({currentuser:req.currentUser || null}));
 export {router as currentUserRouter}
