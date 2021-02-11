a3aacb40ba913af00278352a69b9f1ffc8486294
Abderrahmenla <abdo.vic@hotmail.com>
2021-02-11 12:17:51 +0100

    faking authentification

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -18,16 +18,47 @@ it('can only be accessed if the user is signed in', async () => {
 it('returns a status other than 401 if the user is signed in',async () => {
   const response = await request(app)
     .post('/api/tickets')
+    .set('Cookie',global.signin())
     .send({});
   expect(response.status).not.toEqual(401);
 })
 
 it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400);
   
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10
+    })
+    .expect(400);
 })
 
 it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'azerazer',
+      price: -10
+    })
+    .expect(400);
   
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'sdfgsdfgsd'
+    })
+    .expect(400);
 })
 
 it('creates a ticket with valid inputs ', async () => {
