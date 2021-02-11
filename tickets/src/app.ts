f52595fab5f76891c96112ca7902a0a8d603d7a7
Abderrahmenla <abdo.vic@hotmail.com>
2021-02-11 17:06:44 +0100

    better error logging

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,8 +1,12 @@
-import express from 'express'
-import { json } from 'body-parser'
-import cookieSession from 'cookie-session'
-import { errorHandler, NotFoundError,currentUser } from '@abderrahmenlh/common'
-import {createTicketRouter} from './routes/new'
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@abderrahmenlh/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show'
+
+
 
 const app = express();
 app.set('trust proxy', true);
@@ -12,14 +16,15 @@ app.use(
     signed: false,
     secure: process.env.NODE_ENV !== 'test',
   })
-)
-
+);
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
+
 app.use(errorHandler);
 
 export { app };
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,28 +1,25 @@
-import request from 'supertest'
-import { app } from '../../app'
-import { Ticket } from '../../models/ticket'
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
-  const response = await request(app)
-    .post('/api/tickets')
-    .send({});
+  const response = await request(app).post('/api/tickets').send({});
+
   expect(response.status).not.toEqual(404);
 });
 
 it('can only be accessed if the user is signed in', async () => {
-  await request(app)
-    .post('/api/tickets')
-    .send({})
-    .expect(401);
+  await request(app).post('/api/tickets').send({}).expect(401);
 });
 
-it('returns a status other than 401 if the user is signed in',async () => {
+it('returns a status other than 401 if the user is signed in', async () => {
   const response = await request(app)
     .post('/api/tickets')
-    .set('Cookie',global.signin())
+    .set('Cookie', global.signin())
     .send({});
+
   expect(response.status).not.toEqual(401);
-})
+});
 
 it('returns an error if an invalid title is provided', async () => {
   await request(app)
@@ -30,52 +27,55 @@ it('returns an error if an invalid title is provided', async () => {
     .set('Cookie', global.signin())
     .send({
       title: '',
-      price: 10
+      price: 10,
     })
     .expect(400);
-  
+
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      price: 10
+      price: 10,
     })
     .expect(400);
-})
+});
 
 it('returns an error if an invalid price is provided', async () => {
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      title: 'azerazer',
-      price: -10
+      title: 'asldkjf',
+      price: -10,
     })
     .expect(400);
-  
+
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      title: 'sdfgsdfgsd'
+      title: 'laskdfj',
     })
     .expect(400);
-})
+});
 
-it('creates a ticket with valid inputs ', async () => {
-  // add in a check to make sure a ticket was saved
+it('creates a ticket with valid inputs', async () => {
   let tickets = await Ticket.find({});
   expect(tickets.length).toEqual(0);
 
+  const title = 'asldkfj';
+
   await request(app)
     .post('/api/tickets')
-    .set('Cookie',global.signin())
+    .set('Cookie', global.signin())
     .send({
-      title: 'asldkfj',
-      price: 20
+      title,
+      price: 20,
     })
-    .expect(201)
+    .expect(201);
+
   tickets = await Ticket.find({});
   expect(tickets.length).toEqual(1);
-  
-})
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
+});
