a81552495c63d6f7695cc9297f3df70450b7cfc9
Abderrahmenla <abdo.vic@hotmail.com>
2021-02-12 21:56:55 +0100

    final test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,8 +5,8 @@ import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@abderrahmenlh/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show'
-
-
+import { indexTicketRouter } from './routes/index'
+import { updateTicketRouter } from './routes/update'
 
 const app = express();
 app.set('trust proxy', true);
@@ -20,6 +20,8 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
