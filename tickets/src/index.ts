bff6fddab78e5b8827e25d8e870d67204b8e48a2
Abderrahmenla <abdo.vic@hotmail.com>
2021-02-19 05:48:03 +0100

    adding env variables for natsClient & clusterId

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,9 +5,11 @@ import { natsWrapper } from './nats-wrapper'
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
   if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
-  
+  if (!process.env.NATS_CLIENT_ID) throw new Error('NATS CLIENT ID must be defined');
+  if (!process.env.NATS_CLUSTER_ID) throw new Error('NATS CLUSTER ID must be defined');
+  if (!process.env.NATS_URL) throw new Error('NATS URL must be defined');
   try {
-    await natsWrapper.connect('ticketing', 'laskttjf', 'http://nats-srv:4222');
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID,process.env.NATS_CLIENT_ID, process.env.NATS_URL);
 
     natsWrapper.client.on('close', async () => {
       console.log('NATS connection closed!');
