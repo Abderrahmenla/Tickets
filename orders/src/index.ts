4820c57c06061408f46a62edd68c7c934b5fcdf5
Abderrahmenla <abdo.vic@hotmail.com>
2021-02-22 23:04:41 +0100

    install the orders dependencies

---
+++ orders/src/index.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+import { app } from './app'
+import { natsWrapper } from './nats-wrapper'
+
+const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
+  if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
+  if (!process.env.NATS_CLIENT_ID) throw new Error('NATS CLIENT ID must be defined');
+  if (!process.env.NATS_CLUSTER_ID) throw new Error('NATS CLUSTER ID must be defined');
+  if (!process.env.NATS_URL) throw new Error('NATS URL must be defined');
+  try {
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID,process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+
+    natsWrapper.client.on('close', async () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });  
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+})
+start();
