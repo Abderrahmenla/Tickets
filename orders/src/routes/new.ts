ac04b94deb2bb3bb2e70750d0f004c2d50400fcd
Abderrahmenla <abdo.vic@hotmail.com>
2021-02-23 13:08:16 +0100

    customizing common,adding order& ticket collection

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from 'express'
+import { requireAuth, validateRequest } from '@abderrahmenlh/common'
+import { body } from 'express-validator'
+
 const router = express.Router();
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+  body('ticketId')
+    .not()
+    .isEmpty()
+    .withMessage('TickeId must be provided'),
+], async (req: Request, res: Response) => {
   res.send({});
 });
 export { router as newOrderRouter };
