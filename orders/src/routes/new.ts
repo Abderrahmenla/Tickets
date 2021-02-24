4714233d3fa92d0cd0204aad407d3d79c2bd984a
Abderrahmenla <abdo.vic@hotmail.com>
2021-02-24 16:32:08 +0100

    adding the orders routes with testing

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -12,6 +12,7 @@ import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
 
 const router = express.Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   '/api/orders',
@@ -29,23 +30,26 @@ router.post(
 
     // Find the ticket the user is trying to order in the database
     const ticket = await Ticket.findById(ticketId);
-    if (!ticket) {
-      throw new NotFoundError();
-    }
+    if (!ticket) throw new NotFoundError();
 
     // Make sure that this ticket is not already reserved
     const isReserved = await ticket.isReserved();
-    if (isReserved) {
-      throw new BadRequestError('Ticket is already reserved');
-    }
+    if (isReserved) throw new BadRequestError('Ticket is already reserved');
 
     // Calculate an expiration date for this order
-
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
     // Build the order and save it to the database
-
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket
+    });
+    await order.save();
     // Publish an event saying that an order was created
 
-    res.send({});
+    res.status(201).send(order);
   }
 );
 
