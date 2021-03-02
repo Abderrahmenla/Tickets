d4cd51242950c4747ca066c417ca7e7302bec05f
Abderrahmenla <abdo.vic@hotmail.com>
2021-03-02 16:59:30 +0100

    fixing

---
+++ auth/node_modules/@abderrahmenlh/common/build/events/types/order-status.d.ts
@@ -0,0 +1,6 @@
+export declare enum OrderStatus {
+    Created = "created",
+    Canceled = "canceled",
+    AwaitingPayment = "awaiting:payment",
+    Complete = "complete"
+}
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -10,6 +10,8 @@ import {
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router();
 const EXPIRATION_WINDOW_SECONDS = 15 * 60;
@@ -48,7 +50,16 @@ router.post(
     });
     await order.save();
     // Publish an event saying that an order was created
-
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticketId,
+        price: ticket.price,
+      }
+    });
     res.status(201).send(order);
   }
 );
