0142ab50ffe88055a20f2bbc322cf01ac04d5cda
Abderrahmenla <abdo.vic@hotmail.com>
2021-03-06 21:32:15 +0100

    fixing

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { Order, OrderStatus } from './order';
-import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
 
 interface TicketAttrs {
   id: string;
@@ -17,6 +17,10 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -40,14 +44,21 @@ const ticketSchema = new mongoose.Schema(
     },
   }
 );
+
 ticketSchema.set('versionKey', 'version');
 ticketSchema.plugin(updateIfCurrentPlugin);
 
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
     _id: attrs.id,
     title: attrs.title,
-    price:attrs.price,
+    price: attrs.price,
   });
 };
 ticketSchema.methods.isReserved = async function () {
