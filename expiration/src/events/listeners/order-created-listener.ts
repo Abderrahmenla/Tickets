b61b56c495b77c1370a2d032e80fe7b662b0eb84
Abderrahmenla <abdo.vic@hotmail.com>
2021-03-10 13:03:26 +0100

    expiration complete test listener

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,11 +1,21 @@
 import { Listener, OrderCancelledEvent, OrderCreatedEvent, Subjects } from '@abderrahmenlh/common'
 import { queueGroupName } from './queue-group-name'
 import { Message } from 'node-nats-streaming'
+import { expirationQueue } from '../../queues/expiration-queue'
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log('Waiting this many milliseconds to process the job:',delay);
     
+    await expirationQueue.add({
+      orderId: data.id
+    }, {
+      delay:delay
+    });
+    msg.ack();
   }
 }
--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from 'bull'
+import { ExpirationCompletePublisher } from '../events/publishers/expiration-complete-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 interface Payload {
   orderId: string;
@@ -11,6 +13,8 @@ const expirationQueue = new Queue<Payload>('order:expiration', {
 })
 
 expirationQueue.process(async (job) => {
-  console.log('I want to publish and expiration:complete event for orderId', job.data.orderId);
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId:job.data.orderId
+  })
 });
 export { expirationQueue };
