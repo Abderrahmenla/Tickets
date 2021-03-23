be8b159b50c0956c071df1f651361fe86550562b
Abderrahmenla <abdo.vic@hotmail.com>
2021-03-23 17:21:33 +0100

    adding the purchase route and testing it

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@abderrahmenlh/common';
+import { createChargeRouter } from './routes/new'
 
 
 const app = express();
@@ -15,7 +16,7 @@ app.use(
   })
 );
 app.use(currentUser);
-
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
