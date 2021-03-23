c711356c598690bec3ba4947ffdb785fd68359ca
Abderrahmenla <abdo.vic@hotmail.com>
2021-03-23 12:42:57 +0100

    auth works well

--- payments/src/events/listeners/order-cancelled-listener.ts
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -1,21 +1,30 @@
-import { OrderCancelledEvent, Subjects, Listener,OrderStatus } from '@abderrahmenlh/common'
+import {
+  OrderCancelledEvent,
+  Subjects,
+  Listener,
+  OrderStatus,
+} from '@abderrahmenlh/common';
+import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
-import { Message } from 'node-nats-streaming'
-import { Order } from '../../models/order'
+import { Order } from '../../models/order';
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
   subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
   queueGroupName = queueGroupName;
+
   async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
     const order = await Order.findOne({
-      __id: data.id,
-      version: data.version - 1
+      _id: data.id,
+      version: data.version - 1,
     });
+
     if (!order) {
       throw new Error('Order not found');
     }
+
     order.set({ status: OrderStatus.Cancelled });
     await order.save();
+
     msg.ack();
-  };
-};
+  }
+}
