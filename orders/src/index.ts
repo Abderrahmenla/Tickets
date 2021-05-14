59a09ba5471636f435dd12ff2df4b925aed61e73
Abderrahmenla <abdo.vic@hotmail.com>
2021-05-14 21:52:01 +0100

    add startup message to order service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,18 +1,21 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-import { TicketCreatedListener } from './events/listeners/ticket-created-listener'
-import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener'
-import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener'
-import { PaymentCreatedListener } from './events/listeners/payment-created-listener'
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
+import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
+import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
+  console.log('Starting...');
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
   if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
-  if (!process.env.NATS_CLIENT_ID) throw new Error('NATS_CLIENT_ID must be defined');
+  if (!process.env.NATS_CLIENT_ID)
+    throw new Error('NATS_CLIENT_ID must be defined');
   if (!process.env.NATS_URL) throw new Error('NATS_URL must be defined');
-  if (!process.env.NATS_CLUSTER_ID) throw new Error('NATS_CLUSTER_ID must be defined');
-  
+  if (!process.env.NATS_CLUSTER_ID)
+    throw new Error('NATS_CLUSTER_ID must be defined');
+
   try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
